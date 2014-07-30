ff8c11e678e89195b488b51bc68d6d8e00d47ee0
EC2 Default User <ec2-user@ip-172-31-15-101.us-west-1.compute.internal>
2014-07-30 22:39:19 +0000

    rails new blog

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = '250949428d8c013df0357ddd1b139648b3f5a49b5f0a3e12a38ad2c1b5ea91bec81cb178d239adcc746b2f216480acd64520bca7a9b05441c259996a9d5baaf3'
